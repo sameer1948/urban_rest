203df4f45bfd9ed5bb50217d207f18c76a24d2c3
Sameer <sameer.sameer185@gmail.com>
2025-07-20 12:48:54 +0530

    Adding Constants  Data Helper class

---
+++ lib/constants/databaseConstants.dart
@@ -0,0 +1,66 @@
+// ignore: file_names
+// ignore_for_file: constant_identifier_names
+class DatabaseConstants {
+  static const String DATABASE_NAME = 'urban.db';
+  static const String TABLE_BED = 'BED';
+  static const String TABLE_CUSTOMER = 'CUSTOMER';
+  static const String TABLE_BOOKING = 'BOOKING';
+  static const String TABLE_INVOICE = 'INVOICE';
+  static const String TABLE_BED_RATE = 'BED_RATE';
+
+  /// SQL statement for Total Rows of table Bed.
+  static const String SELECT_COUNT_FROM = '''SELECT COUNT(*) FROM ''';
+
+  /// SQL statement to create the beds table.
+  static const String CREATE_TABLE_BED = '''
+    CREATE TABLE $TABLE_BED (
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      status TEXT NOT NULL CHECK (status IN ('occupied', 'available'))
+    )
+    ''';
+
+  /// SQL statement to create the customers table.
+  static const String CREATE_TABLE_CUSTOMER = '''
+    CREATE TABLE $TABLE_CUSTOMER (
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      name TEXT NOT NULL, 
+      phone TEXT NOT NULL UNIQUE, 
+      address TEXT NOT NULL, 
+      securityId TEXT NOT NULL
+    )
+    ''';
+
+  /// SQL statement to create the Booking table.
+  static const String CREATE_TABLE_BOOKING = '''
+    CREATE TABLE $TABLE_BOOKING (
+      id INTEGER PRIMARY KEY,
+      bedId INTEGER NOT NULL,
+      customerId INTEGER NOT NULL,
+      startTime TEXT NOT NULL,
+      endTime TEXT NOT NULL,    
+      FOREIGN KEY (bedId) REFERENCES $TABLE_BED(id),
+      FOREIGN KEY (customerId) REFERENCES $TABLE_CUSTOMER(id)
+      UNIQUE (customerId, bedId, startTime)
+    )
+    ''';
+
+  /// SQL statement to create the Invoice table.
+  static const String CREATE_TABLE_INVOICE = '''
+    CREATE TABLE $TABLE_INVOICE (
+      id INTEGER PRIMARY KEY,
+      bookingId INTEGER NOT NULL,
+      amount REAL NOT NULL,
+      paymentDate TEXT NOT NULL,
+      isPaid INTEGER NOT NULL,
+      FOREIGN KEY (bookingId) REFERENCES $TABLE_BOOKING(id)
+    )
+    ''';
+
+  /// SQL statement to create the Bill Rate table.
+  static const String CREATE_TABLE_BED_RATE = '''
+    CREATE TABLE $TABLE_BED_RATE (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      pricePerHour REAL NOT NULL
+    )
+    ''';
+}
---
+++ lib/database/databaseHelper.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: file_names
+
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:urban_rest/constants/databaseConstants.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  factory DatabaseHelper() => _instance;
+  DatabaseHelper._internal();
+
+  static Database? _database;
+
+  /// Returns the singleton instance of the database.
+  /// If the database is not initialized, it will be created.
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    // Initialize database if not already initialized
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  // Initialize the database
+  Future<Database> _initDatabase() async {
+    try {
+      // Get the application's document directory
+      Directory documentsDirectory = await getApplicationDocumentsDirectory();
+      String path = join(
+        documentsDirectory.path,
+        DatabaseConstants.DATABASE_NAME,
+      );
+
+      // Open the database (create if it doesn't exist)
+      return await openDatabase(
+        path,
+        version: 1,
+        onConfigure: (db) async {
+          await db.execute('PRAGMA foreign_keys = ON');
+        },
+        onCreate: _onCreate,
+        //onUpgrade: _onUpgrade,
+      );
+    } catch (e) {
+      print("Error opening database: $e");
+      rethrow; // You can handle the error more gracefully here.
+    }
+  }
+
+  // Create tables when the database is created for the first time
+  Future<void> _onCreate(Database db, int version) async {
+    try {
+      await db.execute(DatabaseConstants.CREATE_TABLE_BED);
+      await db.execute(DatabaseConstants.CREATE_TABLE_CUSTOMER);
+      await db.execute(DatabaseConstants.CREATE_TABLE_BOOKING);
+      await db.execute(DatabaseConstants.CREATE_TABLE_INVOICE);
+      await db.execute(DatabaseConstants.CREATE_TABLE_BED_RATE);
+    } catch (e) {
+      print("Error creating tables: $e");
+      rethrow;
+    }
+  }
+
+  // // Handle database upgrades (e.g., schema changes) when version is incremented
+  // Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
+  //   if (oldVersion < newVersion) {
+  //     // Example of handling upgrades, add more migration steps as needed.
+  //     print('Upgrading database from version $oldVersion to $newVersion');
+  //     // Example: db.execute("ALTER TABLE ...");
+  //   }
+  // }
+
+  // Close the database when no longer needed
+  Future<void> close() async {
+    if (_database != null) {
+      await _database!.close();
+      _database = null;
+    }
+  }
+}
