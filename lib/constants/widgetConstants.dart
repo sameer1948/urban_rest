30a3e2c56c99c367d1e6e38b16750c6e397cb475
Sameer <sameer.sameer185@gmail.com>
2025-07-21 01:17:38 +0530

    Add & update Constants

--- lib/constants/widgetConstants.dart
+++ lib/constants/widgetConstants.dart
@@ -1,5 +1,13 @@
 // ignore_for_file: file_names, constant_identifier_names
 
+import 'package:intl/intl.dart';
+
 class Widgetconstants {
   static const String APPLICATION_ICON = 'assets/icon/icon.png';
+  static const String BED_ICON = 'assets/svg/bed.svg';
+
+  static int formatDateTimeToInt() {
+    final String formatted = DateFormat('yyMMddHHmmss').format(DateTime.now());
+    return int.parse(formatted);
+  }
 }
